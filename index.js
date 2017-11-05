61d6d28bf20a1b656a468cacd548e7ad2482e7da
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-05 13:39:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor(Math.random * 100 + 1);
+  var price = Math.floor(Math.random() * 100 + 1);
   var obj = {};
   obj[item] = price;
   cart.push(obj);
