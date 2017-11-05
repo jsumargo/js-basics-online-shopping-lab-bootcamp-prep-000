a2408a0cc42d9d240272d31aa343099ffc6a1fd8
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-05 13:42:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ function viewCart() {
 
 function total() {
   // write your code here
+  var total = 0;
+  for(var i = 0; i < cart.length; i++) {
+    total += cart[i][1];
+  }
+  return total;
 }
 
 function removeFromCart(item) {
